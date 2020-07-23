c147683f04005c2fef5a861cd68cba0502fc802d
theSohanur <sohanur064@gmail.com>
2020-07-23 06:08:00 +0600

    implement get min value

---
+++ Binary_search.py
@@ -0,0 +1,33 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+        else:
+            self.insertNode(data, self.root)
+
+    def insertNode(self,data,node):
+        if data < node.data:
+            if node.leftChild:
+                self.insertNode(data,node.leftChild)
+            else:
+                node.rightChild = Node(data)
+
+    def getMinValue(self):
+        if self.root:
+            return  self.getMin(self.root)
+
+    def getMin(self,node):
+        if node.leftChild:
+            return self.getMin(node.leftChild)
+        return node.data
+
