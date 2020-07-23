a0ac07ef95e9cfa5f82311c8c855862b63f3b67f
theSohanur <sohanur064@gmail.com>
2020-07-23 06:22:36 +0600

    def max value

--- Binary_search.py
+++ Binary_search.py
@@ -15,19 +15,26 @@ class BinarySearchTree(object):
         else:
             self.insertNode(data, self.root)
 
-    def insertNode(self,data,node):
+    def insertNode(self, data, node):
         if data < node.data:
             if node.leftChild:
-                self.insertNode(data,node.leftChild)
+                self.insertNode(data, node.leftChild)
             else:
                 node.rightChild = Node(data)
 
     def getMinValue(self):
         if self.root:
-            return  self.getMin(self.root)
+            return self.getMin(self.root)
 
-    def getMin(self,node):
+    def getMin(self, node):
         if node.leftChild:
             return self.getMin(node.leftChild)
         return node.data
 
+    def getMaxValue(self):
+        if self.root:
+            return self.getMax(self.root)
+
+    def getMax(self, node):
+        if node.rightChild:
+            return self.getMax(node.rightChild)
